28605c44c81f781df825a523082f703a88a5cc1b
Wilcas <wcastro@misena.edu.co>
2019-09-25 17:03:41 -0500

    Actualizacion JS

---
+++ scriptUno.js
@@ -0,0 +1,38 @@
+// alert("Como va todo?")
+
+//Valor absoluto de un numero
+// function valAbsoluto(numero)
+// {
+//     return numero;
+// }
+
+// var saludo = function(nombre){
+//     return "Hola " + nombre;
+// }
+
+//En la consola: saludo("Developer");
+
+//Serie fibonacci
+// function fibonacci(numero)
+// {
+//     var var1 = 0;
+//     var var2 = 1;
+//     var var3;
+ 
+//     document.write(var1+"");
+//     document.write(var2+"");
+ 
+//     for(var i=3; i <= numero;i++)
+//     {
+//         var3 = var1 + var2;
+//         var1 = var2;
+//         var2 = var3;
+//         document.write(var3+"");
+//     }
+// }
+
+//Condicionales
+
+//switch
+
+//Loops
