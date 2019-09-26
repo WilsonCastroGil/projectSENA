8b6a7b5c5edb2c2d183cbc7ffd054ff8ad288938
Wilcas <wcastro@misena.edu.co>
2019-09-26 10:01:16 -0500

    crear y leer json

--- ejemplos/ejDos/js/scriptFormatos.js
+++ ejemplos/ejDos/js/scriptFormatos.js
@@ -1,4 +1,5 @@
 // JSON: javascript object notation
+/* //Leer un archico JSON
 var data = '[{"name":"Aragorn", "race":"Human"},{"name":"Gimli","race":"Dwarf"}]'
 console.log(data)
 
@@ -12,4 +13,24 @@ console.log(data[1].name)
 for (let i = 0; i < data.length; i++) {
     console.log(data[i].name + ' is a ' + data[i].race + '.')
     console.log('El personaje ' + data[i].name + ' es de raza ' + data[i].race)
+} */
+
+// Crear y Leer un archivo JSON
+var request = new XMLHttpRequest()
+
+//Abrir la solicitud y sera tipo get. data.json es el archivo con la carpeta donde esta guardado
+request.open('GET', 'json/data.json', true)
+
+//Crear una funcion cuando el archivo este cargado
+request.onload = function () {
+    var data = JSON.parse(this.response)
+    console.log(data)
+    
+    //iterar sobre los datos para imprimirlos (mostrarlos o pintarlos)
+    for (let i=0; i<data.length; i++) {
+        console.log(" El personaje " + data[i].name + ' es de raza ' + data[i].race)        
+    }
 }
+
+//Enviar el request
+request.send();
